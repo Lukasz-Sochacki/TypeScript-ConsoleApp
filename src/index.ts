246f43d4a3570043ae0ddfeef7aa21c27f8e5b1b
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-27 10:29:56 +0100

    Add UsersData class

--- src/index.ts
+++ src/index.ts
@@ -51,6 +51,59 @@ class Message {
 // Message.showColorized(MessageVariant.Error, 'Test 2'); // "x Test 2"
 // Message.showColorized(MessageVariant.Info, 'Test 3'); // ℹ "Test 3"
 
+interface User {
+  name: string;
+  age: number;
+}
+
+class UsersData {
+  public data: User[] = [];
+
+  public showAll(): void {
+    Message.showColorized(MessageVariant.Info, 'Users data');
+
+    if (this.data.length > 0) {
+      console.table(this.data);
+    } else {
+      console.log('No data...');
+    }
+  }
+
+  public add(user: User): void {
+    if (user.age > 0 && user.name.length > 0) {
+      this.data.push(user);
+      Message.showColorized(
+        MessageVariant.Success,
+        'User has been successfully added!',
+      );
+    } else {
+      Message.showColorized(MessageVariant.Error, 'Wrong data!');
+    }
+  }
+
+  public remove(name: string): void {
+    const index = this.data.findIndex((user) => user.name === name);
+
+    if (index !== -1) {
+      this.data.splice(index, 1);
+      Message.showColorized(MessageVariant.Success, 'User deleted!');
+    } else {
+      Message.showColorized(MessageVariant.Error, 'User not found...');
+    }
+  }
+}
+
+// const users = new UsersData();
+// users.showAll();
+// users.add({ name: 'Jan', age: 20 });
+// users.add({ name: 'Adam', age: 30 });
+// users.add({ name: 'Kasia', age: 23 });
+// users.add({ name: 'Basia', age: -6 });
+// users.showAll();
+// users.remove('Maurycy');
+// users.remove('Adam');
+// users.showAll();
+
 enum Action {
   List = 'list',
   Add = 'add',
