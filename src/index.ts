a00f34074771ceb46f8074dc7823a2175c4112b2
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-03-27 10:12:33 +0100

    Add Message class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,55 @@
 const inquirer = require('inquirer');
+import consola from 'consola';
+
+enum MessageVariant {
+  Success = 'success',
+  Error = 'error',
+  Info = 'info',
+}
+
+class Message {
+  constructor(private content: string) {}
+
+  public show(): void {
+    console.log(this.content);
+  }
+
+  public capitalize(): void {
+    this.content =
+      this.content.charAt(0).toUpperCase() +
+      this.content.slice(1).toLowerCase();
+  }
+
+  public toUpperCase(): void {
+    this.content = this.content.toUpperCase();
+  }
+
+  public toLowerCase(): void {
+    this.content = this.content.toLowerCase();
+  }
+
+  public static showColorized(variant: MessageVariant, text: string): void {
+    if (variant === MessageVariant.Success) {
+      consola.success(text);
+    } else if (variant === MessageVariant.Error) {
+      consola.error(text);
+    } else if (variant === MessageVariant.Info) {
+      consola.info(text);
+    }
+  }
+}
+
+// const msg = new Message('heLlo world!');
+// msg.show(); // "heLlo world!"
+// msg.capitalize();
+// msg.show(); // "Hello world!"
+// msg.toLowerCase();
+// msg.show(); // "hello world!"
+// msg.toUpperCase();
+// msg.show(); // "HELLO WORLD!"
+// Message.showColorized(MessageVariant.Success, 'Test'); // √ "Test"
+// Message.showColorized(MessageVariant.Error, 'Test 2'); // "x Test 2"
+// Message.showColorized(MessageVariant.Info, 'Test 3'); // ℹ "Test 3"
 
 enum Action {
   List = 'list',
